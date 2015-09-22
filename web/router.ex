9b73dde073ed03c090eb0b60a097e7a98c9a0567
Kevin Old <kevin@kevinold.com>
2015-09-21 21:26:02 -0500

    add an json api to retrieve users

--- web/router.ex
+++ web/router.ex
@@ -14,6 +14,12 @@ defmodule Support.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api", Support do
+    pipe_through :api
+
+    resources "/users", UserController, except: [:new, :create, :edit, :delete]
+  end
+
   scope "/", Support do
     pipe_through :browser # Use the default browser stack
 
