69b5151e7d3708ec207042ad483c07c84c96cab4
Kevin Old <kevin@kevinold.com>
2015-10-05 09:38:17 -0500

    exrm for ansible playbook

--- mix.exs
+++ mix.exs
@@ -35,6 +35,8 @@ defmodule Support.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.1"}]
+     {:comeonin, "~> 1.1"},
+     {:exrm, "~> 0.18.1"}
+   ]
   end
 end
